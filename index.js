d0bb60f36be4b255be8b2b66fabc051e6012fe8c
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-17 18:52:21 +0530

    project

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ document.addEventListener("scroll", function () {
   nav.classList.add("scrolll")
 
 })
+
+const date = new Date();
+const year = date.getFullYear();
+
+
+const year2 =  document.querySelector("#year");
+year2.append(year)
