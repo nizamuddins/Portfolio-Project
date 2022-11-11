3cd989e48ee5aa7720a90357d252c17f3af014f1
nizamuddins <uddinmdnizam61@gmail.com>
2022-11-11 18:16:06 +0530

    project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const nav=document.querySelector(".navbar");document.addEventListener("scroll",function(){nav.classList.add("scrolll")});const date=new Date,year=date.getFullYear(),year2=document.querySelector("#year");year2.append(year)
+
+let width = window.innerWidth;
+let img = document.querySelector(".img-responsive")
+if(width > 500){
+    img.setAttribute("src","images/banner.png");
+
+}else if(width <= 600){
+    img.setAttribute("src","images/banner2.png");
+}
