aa2de28c7ea565c3526ba408919e8a4388f03058
nizamuddins <uddinmdnizam61@gmail.com>
2022-11-13 05:02:54 +0530

    leetcode

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ if(width > 500){
 }else if(width <= 600){
     img.setAttribute("src","images/banner2.png");
 }
+document.addEventListener("scroll",()=>{
+    let off = window.pageYOffset;
+    console.log(off)
+    if(off >= '180'){
+        let home = document.querySelector(".pos");
+        home.style.position = "fixed";
+        if(width<=600){
+    document.querySelector("#About").style.marginTop='400px';
+
+        }else{
+            document.querySelector("#About").style.marginTop='700px';
+
+        }
+        // home.style.zIndex = "-1";
+
+    }else if(off===0){
+        let home = document.querySelector(".pos");
+        home.style.position = "static";
+        document.querySelector("#About").style.marginTop='0px';
+        // home.style.position = "static";
+        // home.style.zIndex = "1";
+
+    }
+
+
+})
