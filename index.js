81f360287af78398ecf85e02dc339a6d1f369ae9
nizamuddins <uddinmdnizam61@gmail.com>
2022-11-15 04:11:25 +0530

    leetcode

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 const nav=document.querySelector(".navbar");document.addEventListener("scroll",function(){nav.classList.add("scrolll")});const date=new Date,year=date.getFullYear(),year2=document.querySelector("#year");year2.append(year)
 
 let width = window.innerWidth;
-let img = document.querySelector(".img-responsive")
+let off = document.querySelector(".offcanvas");
+let off2 = document.querySelector(".media1");
+
 if(width > 500){
-    img.setAttribute("src","images/banner.png");
-
+    off.classList.remove("text-start");
+    off2.style.position='absolute';
+    off2.style.marginTop= '50px';
+    off2.style.width='10px';
 }else if(width <= 600){
-    img.setAttribute("src","images/banner2.png");
+off.classList.add("text-start");
+off2.style.position='static';
+off2.style.marginTop= '10%';
+off2.style.width='100%';
+
+
 }
-// document.addEventListener("scroll",()=>{
-//     let off = window.pageYOffset;
-//     console.log(off)
-//     if(off >= '150'){
-//         let home = document.querySelector(".pos");
-//         // home.style.position = "fixed";
-//         if(width<=600){
-//         document.querySelector("#About").style.marginTop='400px';
-
-//         }else{
-//             document.querySelector("#About").style.marginTop='700px';
-
-//         }
-//         // home.style.zIndex = "-1";
-
-//     }else if(off===0){
-//         let home = document.querySelector(".pos");
-//         // home.style.position = "static";
-//         document.querySelector("#About").style.marginTop='0px';
-//         // home.style.position = "static";
-//         // home.style.zIndex = "1";
-
-//     }
-
-
-// })
